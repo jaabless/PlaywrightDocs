9a39854399841b74acea3f2c06ee11bc9de00360
Jeffery Anyimadu <jefferyasamoahbless@gmail.com>
2026-02-13 14:52:44 +0000

    feat(utils): implement centralized logging utility using SLF4J

---
+++ src/test/java/utils/LoggerUtil.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtil {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(LoggerUtil.class);
+
+    public static void info(String message) {
+        logger.info(message);
+    }
+}
